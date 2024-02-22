71d9acaecc808beb041e3522db7340331a5a3f0c
Shraya Pal <palshraya@berkeley.edu>
2024-02-22 00:46:41 -0800

    trying to add a determinant tracker

--- Matrix_Rank/src/MatrixConstructor.java
+++ Matrix_Rank/src/MatrixConstructor.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.lang.Math;
 
 public class MatrixConstructor {
     public int row;
     public int col;
     public ArrayList<ArrayList<Double>> values;
+    public static HashMap<ArrayList<Integer>, Double> determinantTracker = new HashMap<>();
+    public static ArrayList<Integer> withoutIndices;
 
     //assumes correct dimensions are passed in
     public MatrixConstructor(int r, int c, double[] v) {
         this.row = r;
         this.col = c;
+        determinantTracker.clear(); // janky
         this.values = new ArrayList<ArrayList<Double>>();
         for (int i = 0; i < this.row; i++) {
             ArrayList<Double> rowvals = new ArrayList<Double>();
@@ -24,6 +28,7 @@ public class MatrixConstructor {
         this.row = r;
         this.col = c;
         this.values = v;
+        determinantTracker.clear();
     }
 
     // creates array values for N x N identity matrix
@@ -56,19 +61,31 @@ public class MatrixConstructor {
         double determinant = 0.0;
         if (this.values.size() == 1) {
             determinant = determinant + this.values.get(0).get(0);
+
         }
         else{
             for (int i = 0; i < this.values.size(); i++) {
                 // cofactor expansion --> runtime analysis: https://informatika.stei.itb.ac.id/~rinaldi.munir/Matdis/2016-2017/Makalah2016/Makalah-Matdis-2016-051.pdf
-                determinant = determinant + (Math.pow(-1, i%2) * (this.values.get(0).get(i) * this.ExcludingSubMatrix(0, i).Determinant()));
+                MatrixConstructor subMatrix = ExcludingSubMatrix(0, i);
+                withoutIndices.add(i);
+                double subMatrixDet = subMatrix.Determinant();
+
+                // myKey = withoutIndices
+
+                determinant = determinant + (Math.pow(-1, i%2) * (this.values.get(0).get(i) * subMatrixDet));
             }
         }
+        determinantTracker.put(withoutIndices, determinant);
+        withoutIndices.remove(withoutIndices.size()-1);
         return determinant;
     }
 
     // calculates rank of matrix, assumes matrix is square
     public int Rank(){
-        
+        if (this.Determinant() != 0.0){
+            return this.row;
+        }
+
     }
 
 }
